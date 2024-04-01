60bcbb613f5b589642552acaf08c9d6ee8758462
prakash <prakashtech250@gmail.com>
2024-04-01 22:48:13 +0545

    apr1

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme";
+import Sidebar from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body className='flex items-start justify-between'>
+          <Sidebar />
+          <main className="w-full h-full">
+            {children}
+          </main>
+        </body>
+      </html>
+    </>
   );
 }
---
+++ frontend/components/Sidebar.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='w-[300px] border-r min-h-screen p-4'>
+        Sidebar
+
+    </div>
+  )
+}
+
+export default Sidebar
