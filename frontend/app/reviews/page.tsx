3bbc9460a1e08d757ce5bf94594e7e20b24ab9d1
prakash <prakashtech250@gmail.com>
2024-04-10 00:13:06 +0545

    apr10

--- frontend/app/reviews/page.tsx
+++ frontend/app/reviews/page.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+"use client"
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import React, { FormEvent } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 const reviews = () => {
+  const [reviews, setReviews] = useState([])
+  const [asin, setAsin] = useState("");
+
+  const fetchRevies = async(asin:string) => {
+    try{
+      const response = await axios.get('https://amazonreviews.vercel.app/reviews/us/1546017453')
+      console.log(response)
+    } catch(error){
+      console.log(error)
+    }
+  }
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log(asin)
+    fetchRevies(asin)
+  }
+
   return (
-    <div>reviews</div>
+    <form onSubmit={handleSubmit}>
+      <div className='flex flex-col items-center mt-8 gap-2'>
+        <label className='text-3xl'>Amazon Product Reviews</label>
+        <div className='flex md:w-5/6 w-full gap-2 mt-6'>
+          <Input type="text" value={asin} onChange={(e) => setAsin(e.target.value)} placeholder='Enter Asin or Product Url'/>
+          <Button>Submit</Button>
+        </div>
+      </div>
+    </form>
   )
 }
 
--- frontend/app/search/page.tsx
+++ frontend/app/search/page.tsx
@@ -1,8 +1,18 @@
+"use client"
+
+import { Input } from '@/components/ui/input'
 import React from 'react'
 
 const search = () => {
   return (
-    <div>search</div>
+    <>
+      <div className='flex flex-col gap-2 items-center mt-8'>
+        <h1 className='text-3xl antialiased'>Amazon Product Search</h1>
+        <div className='mt-8 w-full'>
+          <Input type='text' className='w-full' placeholder='Enter Asin code'/>
+        </div>
+      </div>
+    </>
   )
 }
 
